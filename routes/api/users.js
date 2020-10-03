b290527302f2425d4b17b05dc65cf8472e2e4af2
Sergey Lem <sergey.lem@gmail.com>
2020-10-03 23:55:34 +0600

    added jsonwebtoken

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
-const bcrypt =require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
+const { JsonWebTokenError } = require('jsonwebtoken');
 
 //@route    POST api/users
 //@desc     Test route
@@ -45,6 +48,22 @@ router.post('/', [
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
       await user.save();
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSwcret'),
+        { expiresInt: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({token});
+        }
+      )
 
       //return jsonwebtoken
       res.send('User registered');
