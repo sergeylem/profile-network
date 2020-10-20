77d774869ef507956bda8f28c90dafdb9ba18e09
Sergey Lem <sergey.lem@gmail.com>
2020-10-20 23:47:35 +0600

    Refactoring

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -218,11 +218,33 @@ router.put('/experience', [auth, [
   }
 });
 
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience fromm profile
+// @access  Private
+
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save;
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 // @route   PUT api/profile/education
 // @desc    Add profile education
 // @access  Private
 
-router.put('/experience', [auth, [
+router.put('/education', [auth, [
   check('school', 'School is required')
     .not()
     .isEmpty(),
@@ -235,10 +257,10 @@ router.put('/experience', [auth, [
   check('from', 'From date is required')
     .not()
     .isEmpty()
-]], async (req, res) => {
-  try {
+]],
+  async (req, res) => {
     const errors = validationResult(req);
-    if (errors.isEmpty()) {
+    if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() })
     }
 
@@ -265,18 +287,14 @@ router.put('/experience', [auth, [
     try {
       const profile = await Profile.findOne({ user: req.user.id });
       profile.education.unshift(newEdu);
-      await profile.save;
+
+      await profile.save();
+
       res.json(profile);
     } catch (err) {
       console.error(err.message);
-      res.status(500).send('Server Error');
+      res.status(500).send('Server error');
     }
-
-    res.json({ msg: 'User deleted' });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
-  }
-});
+  });
 
 module.exports = router;
