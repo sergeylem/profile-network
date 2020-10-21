12c320206de975b70b539bb9e89900529e86cf88
Sergey Lem <sergey.lem@gmail.com>
2020-10-21 23:39:29 +0600

    added delete education in profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -297,4 +297,27 @@ router.put('/education', [auth, [
     }
   });
 
+// @route   DELETE api/profile/education/:edu_id
+// @desc    Delete education fromm profile
+// @access  Private
+
+router.delete('/education/:edu_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.education
+    .map(item => item.id).indexOf(req.params.edu_id);
+
+    profile.education.splice(removeIndex, 1);
+
+    await profile.save;
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
