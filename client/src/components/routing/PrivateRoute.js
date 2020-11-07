ea377ce6470ef0e427c2660c9bc93dd1e63e81ed
Sergey Lem <sergey.lem@gmail.com>
2020-11-08 01:29:14 +0600

    Added profile into actions and reducer

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -5,16 +5,23 @@ import PropTypes from 'prop-types';
 
 const PrivateRoute = ({ component: Component, auth: { isAuthenticated, loading },
   ...rest }) => (
-    <Route {...rest} render={props => !isAuthenticated && !loading ? (
-      <Redirect to ='/login' />) : (<Component {...props} />)} />
-    )
+    <Route
+      {...rest}
+      render={props =>
+        !isAuthenticated && !loading ? (
+          <Redirect to='/login' />
+        ) : (<Component {...props} />
+          )
+      }
+    />
+  );
 
 PrivateRoute.propTypes = {
   auth: PropTypes.object.isRequired
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = state => ({
   auth: state.auth
-}
+})
 
 export default connect(mapStateToProps)(PrivateRoute);
