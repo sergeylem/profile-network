05ae708faba673fdf83e64dec44d12e609854a23
Sergey Lem <sergey.lem@gmail.com>
2020-11-11 23:45:46 +0600

    Added createProfile function

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-import {GET_PROFILE, PROFILE_ERROR} from './types';
+import { GET_PROFILE, PROFILE_ERROR } from './types';
 
 // Get current user profile
 export const getCurrentProfile = () => async dispatch => {
   try {
-    const res = await axios('/api/profile/me');
+    const res = await axios.get('/api/profile/me');
 
     dispatch({
       type: GET_PROFILE,
@@ -13,7 +13,42 @@ export const getCurrentProfile = () => async dispatch => {
   } catch (err) {
     dispatch({
       type: PROFILE_ERROR,
-      payload: {msg: err.response.statusText, status: err.response.status}
+      payload: { msg: err.response.statusText, status: err.response.status }
+    })
+  }
+}
+
+// Create or upadate profile
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'aplication/json'
+      }
+    }
+
+    const res = await axios.post('/api/profile', formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    })
+    
+    dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created'))
+
+    if (!edit) {
+      history.push('/dashboard')
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
     })
   }
 }
