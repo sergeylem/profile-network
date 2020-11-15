9db70b82fc47621fa795c1060f2b2de6b12759a6
Sergey Lem <sergey.lem@gmail.com>
2020-11-15 22:59:23 +0600

    Added education and experience into dashboard

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import { GET_PROFILE, UPDATE_PROFILE, PROFILE_ERROR } from './types';
-import {setAlert} from '../actions/alert';
+import { GET_PROFILE, UPDATE_PROFILE, PROFILE_ERROR, ACCOUNT_DELETED, CLEAR_PROFILE } from './types';
+import { setAlert } from '../actions/alert';
 
 // Get current user profile
 export const getCurrentProfile = () => async dispatch => {
@@ -34,7 +34,7 @@ export const createProfile = (formData, history, edit = false) => async dispatch
       type: GET_PROFILE,
       payload: res.data
     })
-    
+
     dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'))
 
     if (!edit) {
@@ -69,10 +69,10 @@ export const addExperience = (formData, history) => async dispatch => {
       type: UPDATE_PROFILE,
       payload: res.data
     })
-    
+
     dispatch(setAlert('Experience Added', 'success'))
 
-      history.push('/dashboard')
+    history.push('/dashboard')
   } catch (err) {
     const errors = err.response.data.errors;
 
@@ -102,10 +102,10 @@ export const addEducation = (formData, history) => async dispatch => {
       type: UPDATE_PROFILE,
       payload: res.data
     })
-    
+
     dispatch(setAlert('Education Added', 'success'))
 
-      history.push('/dashboard')
+    history.push('/dashboard')
   } catch (err) {
     const errors = err.response.data.errors;
 
@@ -119,3 +119,60 @@ export const addEducation = (formData, history) => async dispatch => {
     })
   }
 }
+
+//Delete Experience
+export const deleteExperience = id => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/profile/experience/${id}`);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Experience deleted', 'success'));
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+//Delete Education
+export const deleteEducation = id => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/profile/education/${id}`);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Education deleted', 'success'));
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+//Delete Account & Profile
+export const deleteAccount = () => async dispatch => {
+  if (window.confirm('Are you sure to delete account?')) {
+    try {
+      const res = await axios.delete('/api/profile');
+
+      dispatch({ type: CLEAR_PROFILE });
+      dispatch({ type: ACCOUNT_DELETED });
+
+      dispatch(setAlert('Your account has been permanantly deleted'));
+    } catch (err) {
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  }
+};
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,12 +2,15 @@ import React, { Fragment, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { getCurrentProfile } from '../../actions/profile';
+import { getCurrentProfile, deleteAccount } from '../../actions/profile';
 import Spinner from '../layout/Spinner';
-import {DashboardActions} from './DashboardActions';
+import { DashboardActions } from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
 
 const Dashboard = ({
   getCurrentProfile,
+  deleteAccount,
   auth: { user },
   profile: { profile, loading }
 }) => {
@@ -20,8 +23,15 @@ const Dashboard = ({
       <i className='fas fa-user' /> Welcome {user && user.name}
     </p>
     {profile !== null ? (
-      <Fragment> 
-        <DashboardActions /> 
+      <Fragment>
+        <DashboardActions />
+        <Experience experience={profile.experience} />
+        <Education education={profile.education} />
+        <div className="my-2">
+          <button className="btn btn-danger" onClick={() => deleteAccount()}>
+            <i className="fas fa-user-minus" /> Delete My Account
+            </button>
+        </div>
       </Fragment>
     ) : (
         <Fragment>
@@ -36,6 +46,7 @@ const Dashboard = ({
 
 Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
+  deleteAccount: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   profile: PropTypes.object.isRequired
 }
@@ -45,4 +56,6 @@ const mapStateToProps = state => ({
   profile: state.profile
 })
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
+export default connect(
+  mapStateToProps, 
+  { getCurrentProfile, deleteAccount })(Dashboard);
