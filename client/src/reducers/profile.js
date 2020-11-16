b3201b19b76355d40db76b7917b72f849d785f91
Sergey Lem <sergey.lem@gmail.com>
2020-11-16 23:22:12 +0600

    Added get_repos

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -32,6 +32,12 @@ export default function (state = initialState, action) {
         profiles: payload,
         loading: false
       }
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
+        loading: false
+      }
     case PROFILE_ERROR:
       return {
         ...state,
